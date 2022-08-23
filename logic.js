08e7ce867774c195a834f5b8c0d0b715e94bc42e
Christian Fellwock <christian.kucoding@gmail.com>
2022-08-23 18:44:04 -0500

    Constructor classes built

--- logic.js
+++ logic.js
@@ -1,16 +1,23 @@
 // Javascript needs to generate an HTML page 
 const inquirer = require('inquirer')
 const fs = require('fs')
-// const generateMarkDown = require('./Generated File')
+const Manager = require('./Constructors/Manager')
+const Engineer = require('./Constructors/Engineer')
+const Intern = require('./Constructors/Intern')
+
+// const generateFile = require('./Generated File')
 
 // Arrays
 const managerInfo = []
+const engineerInfo = []
+const internInfo = []
+
+// Build team main function
+function buildTeam() {
 
 
-
-
-// manager function
-
+    // manager function
+createManager()
     function createManager() {
         inquirer.prompt([
             {
@@ -22,7 +29,7 @@ const managerInfo = []
                         return true;
                     } else {
                         console.log("Please enter a manager's name.")
-                        return false; 
+                        return false;
                     }
                 }
             },
@@ -32,11 +39,11 @@ const managerInfo = []
                 name: 'managerid',
                 message: "Enter the manager's employee ID#.",
                 validate: manageridInput => {
-                    if (manageridInput === 'num') {
+                    if (manageridInput) {
                         return true;
                     } else {
                         console.log("Please enter a manager's employee ID#.")
-                        return false; 
+                        return false;
                     }
                 }
             },
@@ -50,7 +57,7 @@ const managerInfo = []
                         return true;
                     } else {
                         console.log("Please enter a manager's email address.")
-                        return false; 
+                        return false;
                     }
                 }
             },
@@ -60,50 +67,194 @@ const managerInfo = []
                 name: 'managerphone',
                 message: "Enter a manager's office phone number.",
                 validate: managerphoneInput => {
-                    if (managerphoneInput = 'num') {
+                    if (managerphoneInput) {
                         return true;
                     } else {
                         console.log("Please enter a manager's office phone number.")
-                        return false; 
+                        return false;
+                    }
+                }
+            },
+        ])
+
+            .then((answers) => {
+                const newManager = new Manager(answers.managername, answers.managerid, answers.manageremail, answers.managerphone)
+                managerInfo.push(newManager)
+                console.log(managerInfo);
+                addMembers();
+            });
+        }
+
+    // employee functions
+    function addMembers() {
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'addEmployee',
+                message: "Choose an employee to add:",
+                choices: [
+                    'Engineer',
+                    'Intern',
+                    'Finish'
+                ]
+
+            }]).then((userChoice) => {
+                switch (userChoice.addEmployee) {
+                    case "Engineer":
+                        createEngineer();
+                        break;
+                    case "Intern":
+                        addIntern();
+                        break;
+                    default:
+                        finishTeam();
+                }
+            });
+
+        // Engineer Function
+        function createEngineer() {
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'engineername',
+                    message: "Enter an engineer's name.",
+                    validate: engineernameInput => {
+                        if (engineernameInput) {
+                            return true;
+                        } else {
+                            console.log("Please enter an engineer's name.")
+                            return false;
+                        }
+                    }
+                },
+
+                {
+                    type: 'input',
+                    name: 'engineerid',
+                    message: "Enter an engineer's employee ID#.",
+                    validate: engineeridInput => {
+                        if (engineeridInput) {
+                            return true;
+                        } else {
+                            console.log("Please enter an engineer's employee ID#.")
+                            return false;
+                        }
+                    }
+                },
+
+                {
+                    type: 'input',
+                    name: 'engineeremail',
+                    message: "Enter an engineer's email address.",
+                    validate: engineeremailInput => {
+                        if (engineeremailInput) {
+                            return true;
+                        } else {
+                            console.log("Please enter an engineer's email address.")
+                            return false;
+                        }
+                    }
+                },
+
+                {
+                    type: 'input',
+                    name: 'engineeruser',
+                    message: "Enter an engineer's GitHub user ID.",
+                    validate: engineeruserInput => {
+                        if (engineeruserInput = 'num') {
+                            return true;
+                        } else {
+                            console.log("Please enter an engineer's GitHub user ID.")
+                            return false;
+                        }
+                    }
+                },
+            ])
+    
+            .then((answers) => {
+                engineerInfo.push(answers.engineername, answers.engineerid, answers.engineeremail, answers.engineeruser)
+                console.log(engineerInfo);
+                addMembers();
+            });
+        };
+    };
+
+    // Intern function
+    function createIntern() {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'internname',
+                message: "Enter an intern's name.",
+                validate: internnameInput => {
+                    if (internnameInput) {
+                        return true;
+                    } else {
+                        console.log("Please enter an intern's name.")
+                        return false;
+                    }
+                }
+            },
+
+            {
+                type: 'input',
+                name: 'internid',
+                message: "Enter an intern's employee ID#.",
+                validate: internidInput => {
+                    if (internidInput === 'num') {
+                        return true;
+                    } else {
+                        console.log("Please enter an intern's employee ID#.")
+                        return false;
+                    }
+                }
+            },
+
+            {
+                type: 'input',
+                name: 'internemail',
+                message: "Enter an intern's email address.",
+                validate: internemailInput => {
+                    if (internemailInput) {
+                        return true;
+                    } else {
+                        console.log("Please enter an intern's email address.")
+                        return false;
+                    }
+                }
+            },
+
+            {
+                type: 'input',
+                name: 'internschool',
+                message: "Enter an intern's school.",
+                validate: internschoolInput => {
+                    if (internschoolInput = 'num') {
+                        return true;
+                    } else {
+                        console.log("Please enter an intern's GitHub user ID.")
+                        return false;
                     }
                 }
             },
         ])
 
         .then((answers) => {
-            managerInfo.push(answers.managername, answers.managerid, answers.manageremail, answers.managerphone,)
-            console.log(managerInfo);
-            createTeam();
+            internInfo.push(answers.internname, answers.internid, answers.internemail, answers.internschool)
+            console.log(internInfo);
+            addMembers();
         });
-            
-    }
-    // Need to pull all of this into one new variable called manager, then run the create team function, which should have three functions with a switch statement that wil; add an engineer, add an intern and then generate the HTML once there are no more team members to add.
+    };
 
-    
+};
 
-    // employee functions
-   function createTeam() {
-    inquirer.prompt([
-        {
-            type: 'list',
-            name: 'addEmployee',
-            message: "Which licenses does this project use?",
-            choices: [
-            'Engineer',
-            'Intern',
-            'Done'
-        ]
+// Finish team function to write to HTML
 
-        }]).then((userChoice) => {
-            switch(userChoice.addEmployee) {
-                case "Engineer":
-                    addEngineer();
-                    break;
-                case "Intern":
-                    addIntern();
-                    break;
-                default:
-                    finishTeam();
-            }
-        })
-    }
+
+
+// End of buildTeam function
+
+
+
+// Callback function to initializ app
+buildTeam();
